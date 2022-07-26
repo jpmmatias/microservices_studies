4078532ca65dce1b566c0a4a77f618efa460698e
jpmmatias <jpmmatias2018@outlook.com>
2022-07-26 19:43:03 -0300

    Posts service ready

---
+++ posts/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const { json } = require('express');
+
+const app = express();
+app.use(json());
+
+const posts = {
+	1: { id: 1, title: 'title 1' },
+	2: { id: 2, title: 'title 2' },
+};
+
+app.get('/posts', (req, res) => {
+	res.send(posts).status(200);
+});
+
+app.get('/posts', (req, res) => {
+	const id = randomBytes(4).toString('hex');
+	const { title } = req.body;
+
+	posts[id] = {
+		id,
+		title,
+	};
+
+	res.send().status(201);
+});
+
+app.listen(4000, () => {
+	console.log('Listening on 4000');
+});
