bbb7382b3a52faf1395e98aa40fe5d1b06d3b741
jpmmatias <jpmmatias2018@outlook.com>
2022-07-28 17:48:00 -0300

    Refactoring client coments component

--- client/src/components/CommentsList.js
+++ client/src/components/CommentsList.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
 const CommentsList = ({ comments, post_id }) => {
-	const renderCommments = comments.map((comment) => (
-		<li
-			key={`${post_id}${comment.id}`}
-			style={{ width: '100%', marginBottom: '20px' }}
-		>
-			<div className='card-body'>
-				<h4>{comment.content}</h4>
-			</div>
-		</li>
-	));
+	const renderCommments = comments.map((comment) => {
+		if (comment.status === 'rejected') return null;
+
+		return (
+			<li
+				key={`${post_id}${comment.id}`}
+				style={{ width: '100%', marginBottom: '20px' }}
+			>
+				<div className='card-body'>
+					<h4>
+						{comment.status === 'approved'
+							? comment.content
+							: 'Waiting approval'}
+					</h4>
+				</div>
+			</li>
+		);
+	});
 
 	if (comments.length === 0) {
 		return <h3>No commments created </h3>;
